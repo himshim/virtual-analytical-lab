b9f617b20c92f3e66ed1d648dad98d87749470a7
himshim26 <97538806+himshim@users.noreply.github.com>
2025-12-25 12:09:15 +0530

    Create graph/graphConfig.js

---
+++ graph/graphConfig.js
@@ -0,0 +1,12 @@
+export const graphConfig = {
+  responsive: true,
+  animation: false,
+  scales: {
+    x: {
+      title: { display: true, text: "Time / min" }
+    },
+    y: {
+      title: { display: true, text: "Response" }
+    }
+  }
+};

556e28fea96b1007bc9608c54e64a8e0080b7b3a
himshim26 <97538806+himshim@users.noreply.github.com>
2025-12-25 17:53:04 +0530

    Update hplc.dataGenerator.js

--- instruments/hplc/hplc.dataGenerator.js
+++ instruments/hplc/hplc.dataGenerator.js
@@ -1,12 +1,21 @@
 export function generateHPLCData(state) {
   let data = [];
 
+  // Single compound for now (Step 3 = mixtures)
+  const hydrophobicity = 0.55;
+
+  const baseRT = 2.0;
+  const rt =
+    baseRT +
+    (hydrophobicity * state.column.factor) /
+    (state.mobilePhase.strength * state.flow);
+
   for (let t = 0; t <= 12; t += 0.05) {
     let signal = Math.random() * 0.02;
 
-    const rt = 3 / state.flow + 2;
-
-    signal += Math.exp(-Math.pow(t - rt, 2) / 0.12);
+    signal += Math.exp(
+      -Math.pow(t - rt, 2) / 0.12
+    );
 
     data.push({ x: t, y: signal });
   }
